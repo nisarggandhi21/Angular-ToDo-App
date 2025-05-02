31b720e07091bcfb19f06a63b7d56985ddb250c6
Nisarg Gandhi <nisarggandhi21@gmail.com>
2025-05-03 01:02:33 +0530

    optimize

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,13 +7,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
+
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   // avatar = input.required<string>();
@@ -24,6 +30,6 @@ export class UserComponent {
   // });
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
