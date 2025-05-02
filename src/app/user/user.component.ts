971be86fee9d2042da41dbc09360cf11331d3c96
Nisarg Gandhi <nisarggandhi21@gmail.com>
2025-05-03 00:36:34 +0530

    added Output Decorater

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,8 +7,10 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
@@ -21,5 +23,7 @@ export class UserComponent {
   //   return 'assets/users/' + this.avatar;
   // });
 
-  onSelectUser() {}
+  onSelect() {
+    this.select.emit(this.id);
+  }
 }
