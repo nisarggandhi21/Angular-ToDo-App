7cdb0516241d66e5443b444fc15d3eec1e64b736
Nisarg Gandhi <nisarggandhi21@gmail.com>
2025-05-12 00:28:51 +0530

    Refactor components to remove standalone mode and update module imports

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from './user.model';
-import { CardComponent } from '../shared/card/card.component';
 
 // type User = {
 //   id: string;
@@ -10,10 +9,8 @@ import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
-  standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
-  imports: [CardComponent],
 })
 export class UserComponent {
   // @Input({ required: true }) id!: string;
